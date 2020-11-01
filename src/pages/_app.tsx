aca432fec386c4897082a5cea41a4f4ad3a09a06
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-11-01 17:50:02 -0300

    Adds support to eslint and prettier

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,15 @@
-import type { AppProps } from 'next/app';
-import { ComponentType } from 'react';
-import '../styles/globals.css';
+import type { AppProps } from 'next/app'
+import { ComponentType } from 'react'
+import '../styles/globals.css'
 
 type Props = AppProps & {
   Component: ComponentType & {
-    Layout: ComponentType;
-  };
-};
-
-function MyApp({ Component, pageProps }: Props) {
-  return <Component {...pageProps} />;
+    Layout: ComponentType
+  }
 }
 
-export default MyApp;
+function MyApp({ Component, pageProps }: Props) {
+  return <Component {...pageProps} />
+}
+
+export default MyApp
