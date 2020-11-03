83f97e2b89864f6d60a8b1ccc6f9d54cd961c301
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-11-02 22:53:45 -0300

    Updates home page using SectionHero

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,18 +1,22 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
 
-import Main from '../components/main'
+import { Header } from '@/components'
+import { SectionHero } from '@/containers'
+import { LandingPageProps } from '@/types'
 import client from '../graphql/client'
 import GET_LANDING_PAGE from '../graphql/queries/getLandingPage'
 
-const Home = () => (
-  <div>
+const Home = ({ logo, header }: LandingPageProps) => (
+  <>
     <Head>
-      <title>Create Next App</title>
-      <link rel="icon" href="/favicon.ico" />
+      <title>CONFIA - Compartilhe com segurança</title>
     </Head>
-    <Main />
-  </div>
+    <main>
+      <Header image={logo} title="confia" showMenuButton={false} />
+      <SectionHero {...header} />
+    </main>
+  </>
 )
 
 export const getStaticProps: GetStaticProps = async () => {
