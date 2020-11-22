cf0f0e7d3f1e95f375bf5d106f2b4229dc60538f
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-11-21 23:25:59 -0300

    Adjusts module path resolve for tests

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,9 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.js'],
+  moduleNameMapper: {
+    '^@/components$': '<rootDir>/src/components/index.ts',
+    '^@/containers$': '<rootDir>/src/containers/index.ts',
+    '^@/layouts$': '<rootDir>/src/types/index.ts',
+  },
 }
