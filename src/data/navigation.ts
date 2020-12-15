e06878fc71d872a53d7827d3c71be6fae82f567b
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-12-14 22:52:08 -0300

    Adds menu to homepage

---
+++ src/data/navigation.ts
@@ -0,0 +1,48 @@
+import {
+  NavigationMenuItemProps,
+  NavigationMenuProps,
+} from '../components/navigation-menu'
+
+export default [
+  {
+    name: 'Home',
+    href: '/',
+  },
+  {
+    name: 'Serviços',
+    menuItemLinks: [
+      {
+        name: 'Monitoramento e Checagem',
+        href: '/monitoramento-e-checagem',
+      },
+      {
+        name: 'Apoio à Capacitação',
+        href: '/apoio-a-capacitacao',
+      },
+    ],
+  } as NavigationMenuProps,
+  {
+    name: 'Publicações',
+    href: '/publicacoes',
+  },
+  {
+    name: 'Eventos',
+    href: '/eventos',
+  },
+  {
+    name: 'Links',
+    href: '/links',
+  },
+  {
+    name: 'Instituições',
+    href: '/instituicoes',
+  },
+  {
+    name: 'Patrocinadores',
+    href: '/patrocinadores',
+  },
+  {
+    name: 'Contatos',
+    href: '/contatos',
+  },
+] as NavigationMenuItemProps[]
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { GetStaticProps } from 'next'
 import Head from 'next/head'
-import { Box, Container } from '@material-ui/core'
+import { Box, Container, useMediaQuery } from '@material-ui/core'
 
-import { Header } from '@/components'
+import { Header, NavigationMenu, DrawerMenu } from '@/components'
 import {
   SectionHero,
   SectionAbout,
@@ -13,6 +13,7 @@ import {
 import { LandingPageProps } from '@/types'
 import client from '../graphql/client'
 import GET_LANDING_PAGE from '../graphql/queries/getLandingPage'
+import navigation from '../data/navigation'
 
 const Home = ({
   logo,
@@ -21,26 +22,35 @@ const Home = ({
   sectionSponsors,
   sectionTeam,
   sectionStatistics,
-}: LandingPageProps) => (
-  <>
-    <Head>
-      <title>CONFIA - Compartilhe com segurança</title>
-    </Head>
+}: LandingPageProps) => {
+  const isDesktop = useMediaQuery('(min-width:1120px)')
+  return (
+    <>
+      <Head>
+        <title>CONFIA - Compartilhe com segurança</title>
+      </Head>
 
-    <main>
-      <Header image={logo} title="confia" />
-      <Box marginTop={10} clone>
-        <Container maxWidth="lg">
-          <SectionHero {...header} />
-          <SectionStatistics {...sectionStatistics} />
-          <SectionAbout {...sectionAbout} />
-          <SectionTeam {...sectionTeam} />
-          <SectionSponsors {...sectionSponsors} />
-        </Container>
-      </Box>
-    </main>
-  </>
-)
+      <main>
+        <Header image={logo} title="confia">
+          {isDesktop ? (
+            <NavigationMenu menuItens={navigation} />
+          ) : (
+            <DrawerMenu menuItens={navigation}></DrawerMenu>
+          )}
+        </Header>
+        <Box marginTop={10} clone>
+          <Container maxWidth="lg">
+            <SectionHero {...header} />
+            <SectionStatistics {...sectionStatistics} />
+            <SectionAbout {...sectionAbout} />
+            <SectionTeam {...sectionTeam} />
+            <SectionSponsors {...sectionSponsors} />
+          </Container>
+        </Box>
+      </main>
+    </>
+  )
+}
 
 export const getStaticProps: GetStaticProps = async () => {
   const { landingPage } = await client.request(GET_LANDING_PAGE)
