76e6b7bda6d579cb5250e5c7a62e84bbf1b0998e
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-12-20 16:01:28 -0300

    Fix click on icon

--- src/components/sub-menu/index.tsx
+++ src/components/sub-menu/index.tsx
@@ -1,6 +1,5 @@
 import { useState, useRef, useEffect } from 'react'
 import {
-  Box,
   Popper,
   Grow,
   Paper,
@@ -8,10 +7,10 @@ import {
   MenuList,
   PopperProps,
   ButtonProps,
-  ListItem,
-  ListItemText,
+  Button,
 } from '@material-ui/core'
 import { ExpandLess, ExpandMore } from '@material-ui/icons'
+
 import MenuItemLink, { MenuItemLinkProps } from '../menu-item-link'
 
 export type SubMenuProps = {
@@ -20,7 +19,7 @@ export type SubMenuProps = {
 } & Pick<PopperProps, 'placement'> &
   Pick<ButtonProps, 'color'>
 
-const SubMenu = ({ name, menuItemLinks, placement, color }: SubMenuProps) => {
+const SubMenu = ({ name, menuItemLinks, placement }: SubMenuProps) => {
   const [open, setOpen] = useState(false)
   const prevOpen = useRef(open)
   const anchorRef = useRef<HTMLButtonElement>(null)
@@ -33,12 +32,13 @@ const SubMenu = ({ name, menuItemLinks, placement, color }: SubMenuProps) => {
     prevOpen.current = open
   }, [open])
 
-  const handleToggle = () => {
-    setOpen((prevOpen) => !prevOpen)
-  }
+  const handleToggle = () => setOpen((previousOpen) => !previousOpen)
 
   const handleClose = (event: React.MouseEvent<EventTarget>) => {
-    if (anchorRef?.current?.contains(event.target as HTMLElement)) {
+    if (
+      (event.target as HTMLElement)?.classList?.contains('submenu-icon') ||
+      anchorRef?.current?.contains(event.target as HTMLElement)
+    ) {
       return
     }
 
@@ -53,46 +53,50 @@ const SubMenu = ({ name, menuItemLinks, placement, color }: SubMenuProps) => {
   }
 
   return (
-    <Box>
-      <div>
-        <ListItem
-          button
-          aria-controls={open ? 'menu-list' : undefined}
-          aria-haspopup="true"
-          color={color}
-          onClick={handleToggle}
-        >
-          <ListItemText ref={anchorRef} primary={name} />
-          {open ? <ExpandLess aria-hidden /> : <ExpandMore aria-hidden />}
-        </ListItem>
-        <Popper
-          open={open}
-          anchorEl={anchorRef.current}
-          role={undefined}
-          placement={placement}
-          transition
-          disablePortal
-        >
-          {({ TransitionProps }) => (
-            <Grow {...TransitionProps}>
-              <Paper>
-                <ClickAwayListener onClickAway={handleClose}>
-                  <MenuList
-                    id="menu-list"
-                    autoFocusItem={open}
-                    onKeyDown={handleListKeyDown}
-                  >
-                    {menuItemLinks.map((props) => (
-                      <MenuItemLink key={props.href} {...props} />
-                    ))}
-                  </MenuList>
-                </ClickAwayListener>
-              </Paper>
-            </Grow>
-          )}
-        </Popper>
-      </div>
-    </Box>
+    <>
+      <Button
+        ref={anchorRef}
+        aria-controls={open ? 'menu-list' : undefined}
+        aria-haspopup="true"
+        style={{ textTransform: 'none', fontSize: '1rem', fontWeight: 400 }}
+        onClick={handleToggle}
+        endIcon={
+          open ? (
+            <ExpandLess className="submenu-icon" />
+          ) : (
+            <ExpandMore className="submenu-icon" />
+          )
+        }
+      >
+        {name}
+      </Button>
+      <Popper
+        open={open}
+        anchorEl={anchorRef.current}
+        role={undefined}
+        placement={placement}
+        transition
+        disablePortal
+      >
+        {({ TransitionProps }) => (
+          <Grow {...TransitionProps}>
+            <Paper>
+              <ClickAwayListener onClickAway={handleClose}>
+                <MenuList
+                  id="menu-list"
+                  autoFocusItem={open}
+                  onKeyDown={handleListKeyDown}
+                >
+                  {menuItemLinks.map((props) => (
+                    <MenuItemLink key={props.href} {...props} />
+                  ))}
+                </MenuList>
+              </ClickAwayListener>
+            </Paper>
+          </Grow>
+        )}
+      </Popper>
+    </>
   )
 }
 
