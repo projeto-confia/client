48c3c8c90b9526e2b120a0246963649f5b9f56d9
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-12-20 16:45:33 -0300

    Creates empty contact page

---
+++ src/pages/contatos.tsx
@@ -0,0 +1,36 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { Typography } from '@material-ui/core'
+
+import { LayoutDefault } from '@/components'
+import { LandingPageProps } from '@/types'
+import client from '../graphql/client'
+import GET_LANDING_PAGE from '../graphql/queries/getLandingPage'
+
+const Contatos = ({ logo }: LandingPageProps) => {
+  return (
+    <>
+      <Head>
+        <title>CONFIA - Compartilhe com segurança</title>
+      </Head>
+
+      <LayoutDefault logo={logo}>
+        <Typography component="h1" variant="h1">
+          Contatos
+        </Typography>
+      </LayoutDefault>
+    </>
+  )
+}
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { landingPage } = await client.request(GET_LANDING_PAGE)
+
+  return {
+    props: {
+      ...landingPage,
+    },
+  }
+}
+
+export default Contatos
