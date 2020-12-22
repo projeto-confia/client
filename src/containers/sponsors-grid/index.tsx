43731b5bd98541347b1cf889038e0e53c44675bc
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-12-21 23:07:02 -0300

    Creates component sponsors-grid

---
+++ src/containers/sponsors-grid/index.tsx
@@ -0,0 +1,55 @@
+import {
+  Box,
+  Card,
+  CardActionArea,
+  CardContent,
+  CardMedia,
+  Typography,
+} from '@material-ui/core'
+
+import { SponsorProps } from '../../types/sponsorPage'
+import { getImageUrl } from 'src/utils/getImageUrl'
+
+export type SponsorsGridProps = {
+  sponsors: SponsorProps[]
+}
+
+const SponsorsGrid = ({ sponsors }: SponsorsGridProps) => {
+  const openAnotherTab = (sponsorLink: string) => () =>
+    window.open(sponsorLink, '__blank', 'noreferrer')
+
+  return (
+    <Box
+      component="ul"
+      display="grid"
+      gridTemplateColumns="repeat(auto-fill, minmax(250px, 1fr))"
+      justifyItems="center"
+      gridGap="10px"
+      marginTop={2}
+    >
+      {sponsors.map(({ logo: { alternativeText, url }, name, link }) => (
+        <Box clone key={name} maxWidth={300}>
+          <Card onClick={openAnotherTab(link)} title={link}>
+            <Box clone height="100%">
+              <CardActionArea>
+                <CardMedia
+                  component="img"
+                  alt={alternativeText}
+                  height="300"
+                  image={getImageUrl(url)}
+                />
+                <CardContent>
+                  <Typography variant="h5" component="h2">
+                    {name}
+                  </Typography>
+                </CardContent>
+              </CardActionArea>
+            </Box>
+          </Card>
+        </Box>
+      ))}
+    </Box>
+  )
+}
+
+export default SponsorsGrid
