e7e337101cffbace49d3687c0697efc0eeff7b50
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2020-12-25 21:19:33 -0300

    Creates contributors page

---
+++ src/pages/colaboradores.tsx
@@ -0,0 +1,44 @@
+import { GetStaticProps } from 'next'
+import Head from 'next/head'
+import { Box, Typography } from '@material-ui/core'
+
+import { LayoutDefault } from '@/components'
+import { ContributorsGrid } from '@/containers'
+
+import client from '../graphql/client'
+import GET_CONTRIBUTORS_PAGE from '../graphql/queries/getContributorsPage'
+import { ContributorsPageProps } from '../types/contributorsPage'
+
+const Colaboradores = ({
+  commonPageData: { logo, tabTitle },
+  title,
+  members,
+}: ContributorsPageProps) => (
+  <>
+    <Head>
+      <title>{tabTitle}</title>
+    </Head>
+
+    <LayoutDefault logo={logo}>
+      <Box component="section" p={2}>
+        <Typography component="h1" variant="h2">
+          {title}
+        </Typography>
+
+        <ContributorsGrid contributors={members} />
+      </Box>
+    </LayoutDefault>
+  </>
+)
+
+export const getStaticProps: GetStaticProps = async () => {
+  const { contributorsPage } = await client.request(GET_CONTRIBUTORS_PAGE)
+
+  return {
+    props: {
+      ...contributorsPage,
+    },
+  }
+}
+
+export default Colaboradores
