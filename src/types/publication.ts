8fcfcc18207cab097d27db234204a9cfa29002b1
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2021-01-09 20:38:27 -0300

    Adjusts publication type optional atributtes

--- src/types/publication.ts
+++ src/types/publication.ts
@@ -2,8 +2,8 @@ export type PublicationProps = {
   title: string
   publicationLink: string
   authors: string
-  abstract: string
-  keywords: string
-  publishingInstitution: string
+  abstract?: string
+  keywords?: string
+  publishingInstitution?: string
   publicationDate: string
 }
