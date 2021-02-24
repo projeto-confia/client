b48e0cfb223e016e246b259ca2a65947f3b8a821
Carlos Henriqe da S. Moreira <kmoreira620@gmail.com>
2021-02-24 19:59:55 -0300

    Adjusts SectionOverview component

--- src/containers/section-overview/index.tsx
+++ src/containers/section-overview/index.tsx
@@ -1,37 +1,16 @@
-import Image from 'next/image'
-import { Box, Typography, useTheme } from '@material-ui/core'
+import { Box, Typography } from '@material-ui/core'
 
 import { SectionOverviewProps } from '@/types'
-import React from 'react'
-import { getImageUrl } from '../../utils/getImageUrl'
-import { MediaFormatProps } from 'src/types/media'
 
-const SectionOverview = ({
-  title,
-  description,
-  media: { alternativeText, formats },
-}: SectionOverviewProps) => {
-  const { url, width, height } = formats?.medium as MediaFormatProps
-  const theme = useTheme()
-
-  return (
-    <Box component="section" p={2}>
-      <Typography component="h1" variant="h2">
-        {title}
-      </Typography>
-      <Typography component="p" variant="subtitle1">
-        {description}
-      </Typography>
-      <Box m={`${theme.spacing(3)}px auto`} width={width} maxWidth="100%">
-        <Image
-          src={getImageUrl(url)}
-          alt={alternativeText}
-          width={width}
-          height={height}
-        />
-      </Box>
-    </Box>
-  )
-}
+const SectionOverview = ({ title, description }: SectionOverviewProps) => (
+  <Box component="section" p={2}>
+    <Typography component="h1" variant="h2" gutterBottom>
+      {title}
+    </Typography>
+    <Typography component="p" variant="subtitle1">
+      {description}
+    </Typography>
+  </Box>
+)
 
 export default SectionOverview
