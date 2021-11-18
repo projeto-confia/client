1a0ae1b40d5330ca2c64d2d9c337e3c50adceffc
Carlos Henrique da S. Moreira <kmoreira620@gmail.com>
2021-11-17 21:53:51 -0300

    Allow iframe to be embeded in markdown

--- src/utils/markdownToHtml.ts
+++ src/utils/markdownToHtml.ts
@@ -2,4 +2,6 @@ import DOMPurify from 'isomorphic-dompurify'
 import marked from 'marked'
 
 export const markdownToHtml = (md: string): string =>
-  DOMPurify.sanitize(marked(md))
+  DOMPurify.sanitize(marked(md, { sanitize: false }), {
+    ADD_TAGS: ['iframe'],
+  })
